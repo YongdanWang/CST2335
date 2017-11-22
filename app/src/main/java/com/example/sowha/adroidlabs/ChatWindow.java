4e0f1e7c0f9adf6069557a0d24d725632bf6926e
Yongdan <wang0577@algonquinlinve.com>
2017-11-22 00:06:36 -0500

    Finished lab 4

--- app/src/main/java/com/example/sowha/adroidlabs/ChatWindow.java
+++ app/src/main/java/com/example/sowha/adroidlabs/ChatWindow.java
@@ -4,11 +4,14 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -17,6 +20,7 @@ public class ChatWindow extends Activity {
     Button sendButton;
     EditText chatText;
     ListView chatBox;
+    ArrayList<String> messages = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +31,17 @@ public class ChatWindow extends Activity {
         chatText=(EditText)findViewById(R.id.textbox);
         chatBox=(ListView)findViewById(R.id.chatBox);
 
-        final ArrayList<String> messages = new ArrayList<String>();
+        final ChatAdapter messageAdapter =new ChatAdapter( this );
+        chatBox.setAdapter (messageAdapter);
+
+
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 messages.add(chatText.getText().toString());
+                messageAdapter.notifyDataSetChanged(); //this restarts the process of getCount()/getView();
+                chatText.setText("");
             }
         });
 
@@ -44,11 +53,26 @@ public class ChatWindow extends Activity {
         }
 
         public int getCount(){
-            return super.getCount();
+            return messages.size();
         }
 
-        public String getItem (int position){
-            return super.getItem(position);
+        public String getItem (int position) {
+            return messages.get(position);
+        }
+
+        public View getView (int position, View converView, ViewGroup parent){
+
+            LayoutInflater inflater = ChatWindow.this.getLayoutInflater();
+            View result = null ;
+            if(position%2 == 0)
+                result = inflater.inflate(R.layout.chat_row_incoming, null);
+            else
+                result = inflater.inflate(R.layout.chat_row_outgoing, null);
+
+            TextView message = (TextView)result.findViewById(R.id.messageText);
+            message.setText( getItem(position)); // get the string at position
+            return result;
+
         }
     }
 
